93ae36d1ba71c3c9f5461c45e5ae32bd9d0131ca
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2022-04-26 15:21:21 +0200

    listado pokemons

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController extends AbstractController{
 
-#[Route('/pokemon')]
+#[Route('/pokemon',name:'pokemon')]
 
 public function getPokekmon()
 {
@@ -20,6 +20,35 @@ $pokemon =[
 ]; 
 return $this->render('pokemons/ShowPokemon.html.twig', ['pokemon'=>$pokemon]);
 
+}
+#[Route('/pokemons',name:'listado_pokemons')]
+public function pokemons()
+{
+$pokemons = [
+    [
+        'nombre'=>'Charmander',
+        'description' => 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        'img' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        'id'=> '004'
+        
+    ],
+    [
+        'nombre'=>'Butterfree',
+        'description' => 'In battle, it flaps its wings at great speed to release highly toxic dust into the air.',
+        'img' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png',
+        'id'=> '012'
+        
+    ],
+    [
+        'nombre'=>'Meowth',
+        'description' => 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+        'img' => 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png',
+        'id'=> '052'
+        
+        ]
+    ];
+
+    return $this->render('pokemons/Listpokemons.html.twig',['pokemons'=> $pokemons]);
 }
 
 }
