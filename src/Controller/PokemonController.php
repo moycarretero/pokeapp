c2534da14853e4858d932f182058a40d78e50d0a
BellaMir <bellacontrerascamperos@yahoo.es>
2022-04-27 11:33:24 +0200

    insert documents

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Pokemon;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController extends AbstractController
@@ -25,34 +28,13 @@ class PokemonController extends AbstractController
     }
 
     #[Route("/pokemons",name:"list_de_pokemons")]
-    public function getPokemons()
+    public function getPokemons(EntityManagerInterface $doctrine)
     {
-        $pokemons = [
-            [
-                'nombre' => 'charmander',
-                'descripcion' =>
-                    'Prefiere las cosas calientes. Dicen que cuando llueve le sale vapor de la punta de la cola.',
-                'img' =>
-                    'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
-                'id' => '004',
-            ],
-            [
-                'nombre' => 'Pichu',
-                'descripcion' =>
-                    'A pesar de su pequeño tamaño, puede soltar descargas capaces de electrocutar a un adulto, si bien él también acaba sobresaltado.',
-                'img' =>
-                    'https://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png',
-                'id' => '172',
-            ],
-            [
-                'nombre' => 'Dragonite',
-                'descripcion' =>
-                    'Un Pokémon bondadoso y compasivo al que le resulta imposible dar la espalda a Pokémon o humanos que se encuentren a la deriva.',
-                'img' =>
-                    'https://assets.pokemon.com/assets/cms2/img/pokedex/full/149.png',
-                'id' => '149',
-            ],
-        ];
+        $repositorio = $doctrine->getRepository(Pokemon::class);
+        $pokemons = $repositorio->findAll();
+
+
+
         return $this -> render('pokemons/listPokemons.html.twig',['pokemons'=> $pokemons]);
     }
 
@@ -60,5 +42,26 @@ class PokemonController extends AbstractController
     public function reactPokemons()
     {
         return $this->render("pokemons/reactPokemons.html.twig");
+    }
+
+    #[Route("/insert/pokemons")]
+    public function insertPokemon(EntityManagerInterface $doctrine)
+    {
+        $pokemon1 = new Pokemon();
+        $pokemon1 -> setNombre('Pichu');
+        $pokemon1 -> setDescripcion('Un Pokémon bondadoso y compasivo al que le resulta imposible dar la espalda a Pokémon o humanos que se encuentren a la deriva.');
+        $pokemon1 -> setImagen('https://assets.pokemon.com/assets/cms2/img/pokedex/full/149.png');
+        $pokemon1 -> setCodigo('149');
+
+        $pokemon2 = new Pokemon();
+        $pokemon2 -> setNombre('Dragonite');
+        $pokemon2 -> setDescripcion('A pesar de su pequeño tamaño, puede soltar descargas capaces de electrocutar a un adulto, si bien él también acaba sobresaltado.');
+        $pokemon2 -> setImagen('https://assets.pokemon.com/assets/cms2/img/pokedex/full/172.png');
+        $pokemon2 -> setCodigo('172');
+        $doctrine -> persist($pokemon1);
+        $doctrine -> persist($pokemon2);
+        $doctrine-> flush();
+
+        return new Response("insertados correctamente");
     }
 }
